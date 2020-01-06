453406be920f6f113179d9fcc02e2611f59e89c4
TDNZap <zapzupnz+gitlab@mac.com>
2020-01-07 00:32:23 +1300

    Added Package.swift — now the project is compatible with SPM

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "IRC",
+    platforms: [.macOS(.v10_15)],
+	products: [
+		.library(
+			name: "IRC",
+			targets: ["IRC"])
+	],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "IRC",
+            dependencies: []),
+    ]
+)
