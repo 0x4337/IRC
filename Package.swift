10b709d01fdb57a4b3f9b4ed5f64118f6c4142d6
TDNZap <zapzupnz+gitlab@mac.com>
2020-01-13 16:33:14 +1300

    - Restored Package.swift description version to 5.2

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "IRC",
-    platforms: [.macOS(.v10_14)],
+    platforms: [.macOS(.v10_15)],
     products: [
         .library(
             name: "IRC",
